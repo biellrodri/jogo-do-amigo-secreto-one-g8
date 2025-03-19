b91c35fa6857e329e658782bdcd43335249a3f7f
Gabriel Rodrigues <gabrielsilva.8850@gmail.com>
2025-03-18 23:56:32 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 // Array para armazenar os amigos
-// Array para armazenar os amigos
 let amigos = [];
 
 // Função para adicionar um amigo
